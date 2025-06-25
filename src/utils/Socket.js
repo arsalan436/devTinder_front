076bf97d59f077ea264d0fa565de181449bfdb66
mdarsalan007 <arsalansiddiquie007@gmail.com>
2025-06-25 19:13:45 +0530

    .

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -8,7 +8,5 @@ export const createSocketConnection = (token = null) => {
     reconnectionAttempts: 5,           // Number of reconnection tries
     reconnectionDelay: 2000,           // Wait 2s between attempts
     timeout: 10000,                    // 10s connection timeout
-    withCredentials: true,             // Include cookies if auth via cookies
-    auth: token ? { token } : undefined // Pass token via auth if using JWT auth
   });
 };
