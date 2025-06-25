69e58624622826cc49f7b4c41cf4a22ae73fde06
mdarsalan007 <arsalansiddiquie007@gmail.com>
2025-06-25 18:32:49 +0530

    .

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -8,5 +8,9 @@ export const BASE_URL = "/api";
 // 👇 Base URL for your REST API server (Express)
 // export const BASE_URL = "http://localhost:3000/api";
 
+// local
+// export const SOCKET_URL = "http://localhost:3000";
+
 // 👇 Base URL for your Socket.IO server (attached to Express on the same port)
+// aws
 export const SOCKET_URL = "http://13.60.225.37";
